3627807bc24a85569d33197dda8d20b6f72df224
Paul Rademacher <paul@tastylabs.com>
2014-05-14 23:30:27 -0700

    Map works, with limits

--- lib/map.hpp
+++ lib/map.hpp
@@ -28,10 +28,13 @@ void map(std::vector<T> objects,
   }
 
   struct State {
+    std::function<void()> spawn_one;
     std::vector<T> results;
     std::shared_ptr<State> keep_alive;
     std::atomic<int> results_count;
     unsigned int task_limit;
+    unsigned int task_counter;
+    bool reached_limit;
     const TaskCompletionCallback<T> *final_callback;
     bool had_error = false;
 
@@ -52,30 +55,51 @@ void map(std::vector<T> objects,
   state->final_callback = &final_callback;
   state->results_count.store(0);
   state->task_limit = task_limit;
+  state->task_counter = 0;
+  state->reached_limit = false;
 
-  for (int i = 0; i < objects.size(); i++) {
-    auto object = objects[i];
-    func(object, [i, state](ErrorCode error, T result) {
+  state->spawn_one = [state, &func, &objects]() mutable {
+    int index = state->task_counter;
+    auto object = objects[index];
+
+    if (state->task_counter == state->task_limit - 1) {
+      state->reached_limit = true;
+    }
+
+    state->task_counter++;
+
+    func(object, [index, state](ErrorCode error, T result) {
           if (state->had_error) {
             // There was an error code earlier, so work has finished.
             return;
           }
 
-          state->results[i] = result;
-
+          state->results[index] = result;
           state->results_count++;
-
           if (error != OK) {
             state->had_error = true;
           }
 
           if (state->had_error || state->results_count == state->results.size()) {
-            // All results have been collected.
+            // All results have been collected.  We're done.
 
             (*state->final_callback)(error, state->results);
+            state->spawn_one = [](){};  // Break the circular dependency, allowing 'state'
+                                        // to release.
             state->keep_alive.reset();
+          } else if (state->reached_limit) {
+            // We've spawned as many tasks as our limit in the initial loop.
+            // Now, as each task completes, spawn one more if there are still
+            // tasks on the queue.
+            if (state->task_counter < state->results.size()) {
+              state->spawn_one();
+            }
           }
         });
+  };
+
+  while (state->task_counter < task_limit) {
+    state->spawn_one();
   }
 }
 
